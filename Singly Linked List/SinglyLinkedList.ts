d32e815c391622a0aaae850abde6316ca20a8b9e
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2024-05-17 21:53:18 +0600

    added complexity

--- Singly Linked List/SinglyLinkedList.ts
+++ Singly Linked List/SinglyLinkedList.ts
@@ -1,4 +1,4 @@
-class LinkedListNode  {
+class LinkedListNode {
     value: number;
     next: LinkedListNode | null;
     constructor(value: number) {
@@ -10,8 +10,9 @@ export default class SinglyLinkedList {
     private head: LinkedListNode | null = null;
     private tail: LinkedListNode | null = null;
     private size: number = 0;
-    insertAtHead = (value: number)=> {
-        if(this.head === null) {
+    // complexity - O(1)
+    insertAtHead = (value: number) => {
+        if (this.head === null) {
             this.head = new LinkedListNode(value);
             this.tail = this.head;
         } else {
@@ -20,12 +21,12 @@ export default class SinglyLinkedList {
             this.head = newNode;
         }
         this.size++;
-    }
-    deleteHead = ()=> {
-        if(this.head === null) {
+    };
+    // complexity - O(1)
+    deleteHead = () => {
+        if (this.head === null) {
             return;
-        }
-        else if(this.head.next === null) {
+        } else if (this.head.next === null) {
             this.head = null;
             this.tail = null;
             this.size--;
@@ -33,9 +34,10 @@ export default class SinglyLinkedList {
             this.head = this.head.next;
             this.size--;
         }
-    }
-    insertAtTail = (value: number)=> {
-        if(this.tail === null) {
+    };
+    // complexity - O(1)
+    insertAtTail = (value: number) => {
+        if (this.tail === null) {
             this.head = new LinkedListNode(value);
             this.tail = this.head;
         } else {
@@ -44,38 +46,40 @@ export default class SinglyLinkedList {
             this.tail = newNode;
         }
         this.size++;
-    }
-    deleteTail = ()=> {
-        if(this.head === null) {
+    };
+    // complexity - O(N)
+    deleteTail = () => {
+        if (this.head === null) {
             return;
         }
-        if(this.head.next === null) {
+        if (this.head.next === null) {
             this.deleteHead();
             return;
         }
         let tmp = this.head;
-        while(tmp.next !== null && tmp.next.next != null) {
+        while (tmp.next !== null && tmp.next.next !== null) {
             tmp = tmp.next;
         }
         tmp.next = null;
         this.tail = tmp;
         this.size--;
-    }
-    inserAtIndex = (index: number, value: number)=> {
-        if(index === 0) {
+    };
+    // complexity - O(N)
+    inserAtIndex = (index: number, value: number) => {
+        if (index === 0) {
             this.insertAtHead(value);
             return;
         }
-        if(this.head === null) {
+        if (this.head === null) {
             return;
         }
-        if(index === this.size) {
+        if (index === this.size) {
             this.insertAtTail(value);
             return;
         }
         let tmp = this.head;
-        for(let i=0; i<index-1; i++) {
-            if(tmp.next===null) {
+        for (let i = 0; i < index - 1; i++) {
+            if (tmp.next === null) {
                 return;
             }
             tmp = tmp.next;
@@ -84,41 +88,44 @@ export default class SinglyLinkedList {
         newNode.next = tmp.next;
         tmp.next = newNode;
         this.size++;
-    }
+    };
+    // complexity - O(N)
     deleteAtIndex = (index: number) => {
-        if(index === 0) {
+        if (index === 0) {
             this.deleteHead();
             return;
         }
-        if(this.head === null) {
+        if (this.head === null) {
             return;
         }
-        if(index === this.size-1) {
+        if (index === this.size - 1) {
             this.deleteTail();
             return;
         }
         let tmp = this.head;
-        for(let i=0; i<index-1; i++) {
-            if(tmp.next===null) {
+        for (let i = 0; i < index - 1; i++) {
+            if (tmp.next === null) {
                 return;
             }
             tmp = tmp.next;
         }
-        if(tmp.next !== null && tmp.next.next != null) {
+        if (tmp.next !== null && tmp.next.next !== null) {
             tmp.next = tmp.next.next;
         } else {
             tmp.next = null;
         }
         this.size--;
-    }
-    printLinkedList = ()=> {
+    };
+    // complexity - O(N)
+    printLinkedList = () => {
         let tmp = this.head;
-        while(tmp !== null) {
+        while (tmp !== null) {
             console.log(tmp.value);
-            tmp=tmp.next;
+            tmp = tmp.next;
         }
-    }
-    getSize = ()=> {
+    };
+    // complexity - O(1)
+    getSize = () => {
         return this.size;
-    }
+    };
 }
--- Singly Linked List/main.ts
+++ Singly Linked List/main.ts
@@ -5,7 +5,7 @@ singlyList.insertAtHead(10); // 10
 singlyList.insertAtTail(20); // 10 20
 singlyList.insertAtTail(30); // 10 20 30
 singlyList.insertAtTail(40); // 10 20 30 40
-singlyList.inserAtIndex(1,50); // 10 50 20 30 40
+singlyList.inserAtIndex(1, 50); // 10 50 20 30 40
 singlyList.deleteTail(); // 10 50 20 30
 singlyList.deleteHead(); // 50 20 30
 singlyList.deleteAtIndex(1); // 50 30
@@ -13,4 +13,4 @@ singlyList.deleteHead();
 singlyList.deleteHead();
 singlyList.deleteAtIndex(2);
 singlyList.printLinkedList();
-console.log("Size: ",singlyList.getSize());
+console.log("Size: ", singlyList.getSize());
