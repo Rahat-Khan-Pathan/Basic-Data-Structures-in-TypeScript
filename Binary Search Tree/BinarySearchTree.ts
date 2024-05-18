e3812c0f921ed43fe6edc3a6384444592d1c0dd2
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2024-05-18 12:39:13 +0600

    added insert and delete operation

--- Binary Search Tree/BinarySearchTree.ts
+++ Binary Search Tree/BinarySearchTree.ts
@@ -13,7 +13,7 @@ class TreeNode<T> {
 export default class BinarySearchTree<T> {
     private root: TreeNode<T> | null = null;
     // complexity - O(N)
-    convertToBST = (
+    private convertToBST = (
         inputArray: T[],
         l: number = 0,
         r: number = inputArray.length - 1
@@ -31,23 +31,68 @@ export default class BinarySearchTree<T> {
         return curRoot;
     };
     // complexity - O(NlogN)
-    inputBST = (inputArray: T[])=> {
+    convertBstFromArray = (inputArray: T[])=> {
         inputArray.sort();
         this.convertToBST(inputArray);
     }
-    // complexity - O(N)
-    printLevelOrder = () => {
-        if (this.root === null) return;
-        const queue = new QueueList<TreeNode<T>>();
-        queue.push(this.root);
-        while (!queue.isEmpty()) {
-            const par = queue.front();
-            queue.pop();
-            if (!par) continue;
-            console.log(par.value);
-            if (par.left) queue.push(par.left);
-            if (par.right) queue.push(par.right);
+    // complexity - O(logN);
+    private insertNode = (value: T, curNode: TreeNode<T> | null = this.root): TreeNode<T> | null => {
+        if(curNode === null) {
+            return new TreeNode<T>(value);
         }
+        if(value < curNode.value) {
+            curNode.left = this.insertNode(value, curNode.left);
+        } else {
+            curNode.right = this.insertNode(value, curNode.right);
+        }
+        return curNode;
+    } 
+    // complexity - O(logN);
+    insert = (value: T, curNode: TreeNode<T> | null = this.root) => {
+        this.root = this.insertNode(value);
+    }
+    // complexity - O(logN);
+    private deleteNode = (value: T, curNode: TreeNode<T> | null = this.root): TreeNode<T> | null => {
+        if(curNode === null) {
+            return curNode;
+        }
+        if(value < curNode.value) {
+            curNode.left = this.deleteNode(value, curNode.left);
+            return curNode;
+        } else if(value > curNode.value) {
+            curNode.right = this.deleteNode(value, curNode.right);
+            return curNode;
+        }
+        if(curNode.left === null) {
+            return curNode.right;
+        }
+        else if(curNode.right === null) {
+            return curNode.left;
+        }
+        let succParent = curNode;
+        let succ = curNode.right;
+        while (succ.left !== null) {
+            succParent = succ;
+            succ = succ.left;
+        }
+        curNode.value = succ.value;
+        if (succParent.left === succ)
+            succParent.left = succ.right;
+        else
+            succParent.right = succ.right;
+        
+        return curNode;
+    } 
+    // complexity - O(logN);
+    delete = (value: T, curNode: TreeNode<T> | null = this.root) => {
+        this.root = this.deleteNode(value);
+    }
+    // complexity - O(N)
+    printInOrder = (curNode: TreeNode<T> | null = this.root) => {
+        if (curNode === null) return;
+        this.printInOrder(curNode.left);
+        console.log(curNode.value);
+        this.printInOrder(curNode.right);
     };
     // complexity - O(log(N))
     findNode = (value: T, curNode: TreeNode<T> | null = this.root): boolean => {
--- Binary Search Tree/main.ts
+++ Binary Search Tree/main.ts
@@ -1,10 +1,19 @@
 import BinarySearchTree from "./BinarySearchTree";
 
 const bst = new BinarySearchTree<number>();
-bst.inputBST([20,50,40,10,30,70,80]);
+// bst.convertBstFromArray([20,50,40,10,30,70,80]);
             //           40
             //    20             70
             //10      30     50       80
-// bst.printLevelOrder();
-// console.log(bst.findNode(50)); // true
-console.log(bst.findNode(60)); // false
+bst.insert(20);
+bst.insert(70);
+bst.insert(10);
+bst.insert(30);
+bst.insert(50);
+bst.insert(40);
+bst.insert(80);
+bst.delete(40);
+bst.delete(10);
+bst.printInOrder();
+console.log(bst.findNode(10));
+console.log(bst.findNode(70));
